dd8676c9ed1e936667aef4b159de104d87f662e3
jo-syah <johanssyah@gmail.com>
2018-02-15 16:32:00 +0700

    filtered invalid email address

--- user_upload.php
+++ user_upload.php
@@ -16,7 +16,14 @@ function read_csv($csv_file){
     		$result[1] = preg_replace("/[^a-zA-Z]+/", "", ucfirst(strtolower($result[1]))); 
 			$result[2] = str_replace(' ', '', strtolower($result[2])); //all lowercase for email & remove any whitespace
 			
-			$line_of_text[] = $result;
+			//filter invalid email address
+			if (!filter_var($result[2], FILTER_VALIDATE_EMAIL)) {
+  				print_r("Wrong email format: " . $result[2] . "\n");
+			}
+			else
+			{
+        		$line_of_text[] = $result;
+        	}
     	}
     }
     fclose($file);
